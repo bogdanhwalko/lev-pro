508858ba01e7692a6a334effa6401285df447e9d
bogdanhwalko <36229866+bogdanhwalko@users.noreply.github.com>
2026-04-03 20:52:17 +0300

    deleted block

--- app/views/site/about-us.php
+++ app/views/site/about-us.php
@@ -15,9 +15,6 @@
                     <div class="about-img-three">
                         <div class="video-bx">
                             <img src="images/users/our-services/for-video.jpg" alt="">
-                            <div class="video-play-icon">
-                                <a href="video/levpro.mp4" class="popup-youtube video"><i class="fa fa-play"></i></a>
-                            </div>
                         </div>
                     </div>
                 </div>
@@ -26,19 +23,7 @@
                 <div class="about-contact-box">
                     <div class="dlab-tabs tabs-site-button">
                         <div class="tab-content">
-                            <div id="pressure_washing" class="tab-pane active">
-                                <div class="about-inner-content">
-                                    <h2 class="rotate-text">Pressure Washing</h2>
-                                    <div class="m-b30">
-                                        <img src="images/logo99.png" alt="" width="60%">
-                                    </div>
-                                    <h2 class="title">Revitalize Your Home: Professional Pressure Washing Services</h2>
-                                    <p class="m-b20">Has your home lost its original appeal? Have the facades become dull, and the paving stones are covered in dirt, mold, or moss?</p>
-                                    <p>Our Pressure Washing service is an effective and quick solution for deep cleaning various surfaces.</p>
-                                    <p>We use professional equipment and safe cleaning agents to restore the cleanliness and freshness of your property without damage.</p>
-                                </div>
-                            </div>
-                            <div id="painting" class="tab-pane">
+                            <div id="painting" class="tab-pane active">
                                 <div class="about-inner-content">
                                     <h2 class="rotate-text">Painting</h2>
                                     <div class="m-b30">
@@ -62,24 +47,10 @@
                                     <p>We can additionally insulate walls, significantly increasing your home's energy efficiency and reducing heating costs.</p>
                                 </div>
                             </div>
-                            <div id="gutters" class="tab-pane">
-                                <div class="about-inner-content">
-                                    <h2 class="rotate-text">Gutters</h2>
-                                    <div class="m-b30">
-                                        <img src="images/logo99.png" alt="" width="50%">
-                                    </div>
-                                    <h2 class="title">Reliable Water Protection: Professional Gutter Systems</h2>
-                                    <p class="m-b20">Effective rainwater drainage is key to the longevity of your home. Neglected or improperly installed gutters can lead to damage to your foundation, walls, and landscaping.</p>
-                                    <p>Our company offers comprehensive gutter installation, repair, and maintenance (Gutters) services, ensuring reliable protection of your property from the negative effects of water.</p>
-                                    <p>We assess the condition of your current system, provide recommendations, and help you choose the optimal solution.</p>
-                                </div>
-                            </div>
                         </div>
                         <ul class="nav nav-tabs ">
-                            <li><a data-toggle="tab" href="#pressure_washing" class="active show">1</a></li>
-                            <li><a data-toggle="tab" href="#painting">2</a></li>
-                            <li><a data-toggle="tab" href="#siding">3</a></li>
-                            <li><a data-toggle="tab" href="#gutters">4</a></li>
+                            <li><a data-toggle="tab" href="#painting" class="active show">1</a></li>
+                            <li><a data-toggle="tab" href="#siding">2</a></li>
                         </ul>
                     </div>
                 </div>
